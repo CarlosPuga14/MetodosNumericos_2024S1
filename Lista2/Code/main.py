5c607dbd7461f9c50951e8d9fd5ea649af4f25de
CarlosPuga14 <83289504+CarlosPuga14@users.noreply.github.com>
2024-03-23 18:53:46 -0300

    feat: main function

---
+++ Lista2/Code/main.py
@@ -0,0 +1,13 @@
+"""
+The main code to numerically integrate equations. 
+This code has been used during the course IC639 - Metodos Numericos para Engenharia,
+under the supervision of Prof. Dr. Philippe Devloo and Dr. Giovane Avancini
+
+Created by Carlos Puga - 03/23/2024
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
