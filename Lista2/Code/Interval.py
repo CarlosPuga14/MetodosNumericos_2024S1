05f13a8a2702427f096ae997e010db4e7830f1e2
CarlosPuga14 <83289504+CarlosPuga14@users.noreply.github.com>
2024-03-23 18:56:00 -0300

    feat: Interval class for numerical integration evaluation

---
+++ Lista2/Code/Interval.py
@@ -0,0 +1,12 @@
+"""
+The Interval class operates the main data structure for the numerical integration.
+In this class, parameters such as the integration domain, number of intervals, the function to be integrated,
+the analytic solution, and the numerical method are stored.
+
+Created by Carlos Puga - 03/23/2024
+"""
+from dataclasses import dataclass
+
+@dataclass
+class Interval:
+    pass
