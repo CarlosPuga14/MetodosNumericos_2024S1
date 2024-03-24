16a27b0ce7351c47692aefa36f415d06b0a83a7b
CarlosPuga14 <83289504+CarlosPuga14@users.noreply.github.com>
2024-03-24 17:51:24 -0300

    example of numerical integration

--- Lista2/Code/main.py
+++ Lista2/Code/main.py
@@ -9,21 +9,49 @@ from Interval import Interval
 import numpy as np
 
 def main():
-    interval = Interval(0.0, 1, 2)
+    # defining the integration interval
+    n_divisions = 2
+    npoints = 4
+    interval = Interval(_a = 0.0, _b = 1.0, _n_refinements = n_divisions, _n_points = npoints)
 
-    func = lambda x: np.sin(x)
+    # defining the refinement level (how many intervals are going to be used to integrate the function)
+    ref_level = 2
 
-    ref_level = 1
+    # defining the function to be integrated and the exact solution
+    p = 0
+    func = lambda x: x ** p
+    exact = lambda x: 1/(p + 1) * x ** (p + 1)
 
-    exact = lambda x: -np.cos(x)
-    interval.SetExactSolution(exact, ref_level)
-    
-    interval.SetTrapezoidalRule()
+    printTxt = True
+
+    # defining the numerical method to be used
+    method: int = 3
+    match method:
+        case 0:
+            interval.SetSimpsonOneThirdRule()
+
+        case 1:
+            interval.SetSimpsonThreeEighthsRule()
+
+        case 2:
+            interval.SetTrapezoidalRule()
+
+        case 3:
+            interval.SetGaussLegendreRule()
+
+        case _:
+            raise ValueError("Invalid method")
+
+    # integrating the function
     interval.NumericalIntegrate(func, ref_level)
     
+    # computing the error
+    interval.SetExactSolution(exact, ref_level)
     interval.ComputeError()
 
-    interval.Print()
+    # printing the results
+    results_file = "Test.txt" if printTxt else None
+    interval.Print(file = results_file, print_sub_intervals = True)
 
 if __name__ == "__main__":
     main()
