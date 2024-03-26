49800da3314604efc17fbba6f7e1c2b14d81e617
CarlosPuga14 <83289504+CarlosPuga14@users.noreply.github.com>
2024-03-26 11:19:01 -0300

    uptaded pdf

--- Lista2/Code/main.py
+++ Lista2/Code/main.py
@@ -6,38 +6,55 @@ under the supervision of Prof. Dr. Philippe Devloo and Dr. Giovane Avancini
 Created by Carlos Puga - 03/23/2024
 """
 from Interval import Interval
+from scipy import special
 import numpy as np
+
+epot:int = 4
+method: int = 3
+
+def Function1(x:float)->float:
+    return np.exp(-(x - 1)**2 /10**(-epot))
+
+def IntFunction1(x:float)->float:
+    const = (np.sqrt(10)**epot)
+    return  np.sqrt(np.pi) * (special.erf(const * (-1 + x)))/(2*const)
+
 
 def main():
     # defining the integration interval
-    n_divisions = 2
+    n_divisions = 4
     npoints = 4
-    interval = Interval(_a = 0.0, _b = 1.0, _n_refinements = n_divisions, _n_points = npoints)
+    a = 0.0
+    b = 2.0
 
     # defining the refinement level (how many intervals are going to be used to integrate the function)
-    ref_level = 2
+    ref_level = 4
+
+    interval = Interval(_a = a, _b = b, _n_refinements = n_divisions, _n_points = npoints)
 
     # defining the function to be integrated and the exact solution
-    p = 0
-    func = lambda x: x ** p
-    exact = lambda x: 1/(p + 1) * x ** (p + 1)
+    func = Function1
+    exact = IntFunction1
 
     printTxt = True
 
     # defining the numerical method to be used
-    method: int = 3
     match method:
         case 0:
             interval.SetSimpsonOneThirdRule()
+            results_file = "Simpson13.txt" if printTxt else None
 
         case 1:
             interval.SetSimpsonThreeEighthsRule()
+            results_file = "Simpson38.txt" if printTxt else None
 
         case 2:
             interval.SetTrapezoidalRule()
+            results_file = "Trapezoidal.txt" if printTxt else None
 
         case 3:
             interval.SetGaussLegendreRule()
+            results_file = "Gauss.txt" if printTxt else None
 
         case _:
             raise ValueError("Invalid method")
@@ -47,10 +64,9 @@ def main():
     
     # computing the error
     interval.SetExactSolution(exact, ref_level)
-    interval.ComputeError()
+    interval.ComputeError(ref_level)
 
     # printing the results
-    results_file = "Gauss.txt" if printTxt else None
     interval.Print(file = results_file, print_sub_intervals = True)
 
     return 0
