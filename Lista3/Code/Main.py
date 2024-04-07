199488ec18e6de34b5459dffc6132267ed3ac2dd
CarlosPuga14 <83289504+CarlosPuga14@users.noreply.github.com>
2024-04-06 22:29:07 -0300

    main function

--- Lista3/Code/Main.py
+++ Lista3/Code/Main.py
@@ -4,59 +4,37 @@ Main file for List 3 - Matrices Decomposition
 from Matrix import Matrix
 import numpy as np
 
-INVERSE: callable = np.linalg.inv
+method: dict = {"LU": 0, "LDU": 1, "LLt": 2, "LDLt": 3}
 
 def Main()->None:
     matrixtype = 2
 
-    if matrixtype == 0 or matrixtype == 1:
-        # ---- LU and LDU -----
+    if matrixtype == method["LU"]:
         myMatrix = Matrix([[1.0, 3.0, 5.0], [7.0, 9.0, 2.0], [4.0, 6.0, 8.0]])
-        myMatrix.SetDecomposition("LDU")
-        myMatrix.Decompose()
+        myMatrix.SetDecompositionMethod("LU")
+        output_file = "LU.txt"
 
-        print(f"Matrix A: \n{myMatrix.A}")
-        print(f"Matrix L: \n{myMatrix.L}")
-        print(f"Matrix U: \n{myMatrix.U}")
-        print(f"Matrix D: \n{myMatrix.D}")
-
-        if myMatrix.decomposition_type == "LU":
-            print("LU Decomposition:")
-            print(INVERSE(myMatrix.A) - INVERSE(myMatrix.L @ myMatrix.U))
-
-        elif myMatrix.decomposition_type == "LDU":
-            print("LDU Decomposition:")
-            print(INVERSE(myMatrix.A) - INVERSE(myMatrix.L @ myMatrix.D @ myMatrix.U))
+    elif matrixtype == method["LDU"]:
+        myMatrix = Matrix([[1.0, 3.0, 5.0], [7.0, 9.0, 2.0], [4.0, 6.0, 8.0]])
+        myMatrix.SetDecompositionMethod("LDU")
+        output_file = "LDU.txt"
     
-    elif matrixtype == 2:
-        # ------ LLt -----
-        posdefMatrix = Matrix([[23., 10., 9.], [10., 54., 8.], [9., 8., 49.]])
-        posdefMatrix.SetDecomposition("LLt")
-        posdefMatrix.Decompose()
+    elif matrixtype == method["LLt"]:
+        myMatrix = Matrix([[23., 10., 9.], [10., 54., 8.], [9., 8., 49.]])
+        myMatrix.SetDecompositionMethod("LLt")
+        output_file = "LLt.txt"
 
-        print(f"Matrix A: \n{posdefMatrix.A}")
-        print(f"Matrix L: \n{posdefMatrix.L}")
-        print(f"Matrix L.T: \n{posdefMatrix.U}")
-
-        print("LLt Decomposition:")
-        print(INVERSE(posdefMatrix.A) - INVERSE(posdefMatrix.L @ posdefMatrix.U))
-
-    elif matrixtype == 3:
-        # ------ LDLt -----
-        symMatrix = Matrix([[2., 10., 9.], [10., 18., 8.], [9., 8., 16.]])
-        symMatrix.SetDecomposition("LDLt")
-        symMatrix.Decompose()
-
-        print(f"Matrix A: \n{symMatrix.A}")
-        print(f"Matrix L: \n{symMatrix.L}")
-        print(f"Matrix L.T: \n{symMatrix.U}")
-        print(f"Matrix D: \n{symMatrix.D}")
-
-        print("LDLt Decomposition:")
-        print(INVERSE(symMatrix.A) - INVERSE(symMatrix.L @ symMatrix.D @ symMatrix.L.T))
+    elif matrixtype == method["LDLt"]:
+        myMatrix = Matrix([[2., 10., 9.], [10., 18., 8.], [9., 8., 16.]])
+        myMatrix.SetDecompositionMethod("LDLt")
+        output_file = "LDLt.txt"
 
     else:
         raise Exception("Invalid matrix type")
+    
+    myMatrix.Decompose()
+    myMatrix.FindInverse()
+    myMatrix.Print(output_file)
 
 if __name__ == "__main__":
     Main()
