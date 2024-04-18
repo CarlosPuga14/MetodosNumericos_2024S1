5aff86402405838d4fc9b7910a537131c706fe1f
CarlosPuga14 <83289504+CarlosPuga14@users.noreply.github.com>
2024-04-18 12:21:52 -0300

    main function updated to list 3 exercises

--- Lista3/Code/Main.py
+++ Lista3/Code/Main.py
@@ -3,13 +3,21 @@ Main file for List 3 - Matrices Decomposition
 
 Created by Carlos Puga - 04/05/2024
 """
-import sys
 import numpy as np  
-from Matrix import Matrix
+from FullMatrix import FullMatrix
 from SparseMatrix import SparseMatrix
 
 def Main()->None:
-    full_matrix = [
+    decomposition = "LDLt"
+    pivoting = True
+    diagonal = False
+
+    output_file = f"{decomposition}" 
+    output_file += f"{'_Pivoting' if pivoting else ''}"
+    output_file += f"{'_Diagonal' if diagonal else ''}"
+    output_file += ".txt"
+
+    matrix = [
         [815.732, 418.604, 0, 420.445, -1., 281.869, 232.076, 397.658, 65.1193, 0, 421.275, 2.34737, 0.0571453, -0.139624, 419.713],
         [418.604, 520.872, 0, 519.833, -1., 488.09, 334.978, 527.169, -9.12066, 0, 520.524, 2.60818, -1.5467, -0.182023, 519.888],
         [0, 0, 0, 0, 0, 0, -1., -1., 1., 1., 0, 0, 0, 0, 0],
@@ -26,45 +34,41 @@ def Main()->None:
         [-0.139624, -0.182023, 0, -0.877493, 0, 0.0776692, 0.555045, 1.49892, -1.09425, 0,-0.183137, 0.0278837, -0.290078, -0.317777, -0.152792],
         [419.713, 519.888, 0, 1.66667e10, -1., 486.978, 335.855, 527.173, -9.90337, 0, 519.552, 3.30254, -0.813864, -0.152792, 3.33333e10]
     ]
-
-    decomposition = "LU"
-    pivoting = True
     
-    if decomposition == "LU":
-        myMatrix = Matrix([[1.0, 2.0, 8.0], [6.0, 4.0, 7.0], [5.0, 3.0, 9.0]], pivoting)
+    full_matrix = FullMatrix(matrix, pivoting, diagonal, decomposition)
+ 
+    full_matrix.Decompose()
+    full_matrix.FindInverse()
+    full_matrix.Print(output_file)
 
-    elif decomposition == "LDU":
-        myMatrix = Matrix([[1.0, 3.0, 5.0], [7.0, 9.0, 2.0], [4.0, 6.0, 8.0]])
-    
-    elif decomposition == "LLt":
-        myMatrix = Matrix([[23., 10., 9.], [10., 54., 8.], [9., 8., 49.]])
+    full_matrix.CalcMemoryUsage()
+    print(f"{full_matrix.A_Memory = } bytes")
 
-    elif decomposition == "LDLt":
-        myMatrix = Matrix([[2., 10., 9.], [10., 18., 8.], [9., 8., 16.]], pivoting)
-
-    else:
-        raise Exception("Invalid matrix type")
-    
-    myMatrix.SetDecompositionMethod(decomposition)
-    
-    if pivoting:
-        decomposition += "_Pivoting"
-
-    myMatrix.Decompose()
-    myMatrix.FindInverse()
-
-    output_file = f"{decomposition}.txt"
-    myMatrix.Print(output_file)
-
-    vec = np.ones(len(full_matrix))
-    
     sparse_matrix = SparseMatrix()
-    sparse_matrix.ParseFullMatrix(full_matrix)
+    sparse_matrix.ParseFullMatrix(full_matrix.A)
 
-    a = sparse_matrix.Multiply(vec)
-    b = np.dot(full_matrix, vec)
+    # print(f"{sparse_matrix.cols = }")
+    # print(f"{sparse_matrix.data = }")
+    # print(f"{sparse_matrix.ptr = }")
 
-    print(np.allclose(a, b))
+    sparse_matrix.CalcMemoryUsage()
+
+    print(f"{sparse_matrix.data_memory = } bytes")
+    print(f"{sparse_matrix.cols_memory = } bytes")
+    print(f"{sparse_matrix.ptr_memory = } bytes")
+    print(f"{sparse_matrix.total_memory = } bytes")
+
+    # a = sparse_matrix.Multiply(vec)
+    # print(f"{a = }")
+
+    # b = np.dot(full_matrix.A, vec)
+    # print(f"{b = }")
+
+    # print(np.allclose(a, b))
+
+    # sparse_matrix.EvaluateSparsity()
+    # print(f"Sparsity: {sparse_matrix.sparsity:.3%}")
+    # print(f"Density: {sparse_matrix.density:.3%}")
 
 if __name__ == "__main__":
     Main()
