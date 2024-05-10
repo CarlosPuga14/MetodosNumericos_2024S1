d90c92aac9312194c640b4bb70ad28d35c1b1bd2
CarlosPuga14 <83289504+CarlosPuga14@users.noreply.github.com>
2024-05-10 13:13:26 -0300

    Gauss Seidel and SSOR methods implemented

--- Code/List4/Main.py
+++ Code/List4/Main.py
@@ -17,24 +17,79 @@ def ParseVector(file: str)->np.ndarray:
     
     return vector
 
-def Jacobi(matrix:SparseMatrix, rhs:np.array, solini:np.array, niter:int, omega:float)->tuple[float, list[float]]:
+def Jacobi(matrix:SparseMatrix, sol:np.array, res:np.array, omega:float = 1.0)->tuple[float, list[float]]:
     """ 
     Evaluates the Jacobi method for a given matrix, right-hand side and initial solution
     """
+    M = matrix.GetDiagonal()
+    reslocal = res.copy()
+
+    dx = omega * np.divide(reslocal, M)
+    
+    reslocal -= matrix.Multiply(dx)
+    
+    sol += dx
+
+    return sol, reslocal
+
+def GaussSeidelF(matrix:SparseMatrix, sol:np.array, res:np.array, omega:float=1.0)->np.array:
+    """
+    Evaluates the Gauss-Seidel (forward) method for a given matrix, right-hand side and initial solution
+    """
+    size = len(res)
+    dx = np.zeros(size)
+
+    reslocal = res.copy()
+
+    dx[0] = omega * reslocal[0] / matrix.FindAij(0, 0)
+
+    for i in range(1, size):
+        reslocal[i] -= matrix.InnerProductLowerRows(dx, i)
+
+        dx[i] += omega * reslocal[i] / matrix.FindAij(i, i)
+
+    reslocal = res - matrix.Multiply(dx)
+    sol += dx
+
+    return sol, reslocal
+
+def GaussSeidelB(matrix:SparseMatrix, sol:np.array, res:np.array, omega:float=1.0)->np.array:
+    """
+    Evaluates the Gauss-Seidel (backward) method for a given matrix, right-hand side and initial solution
+    """
+    size = len(res)
+    dx = np.zeros(size)
+
+    reslocal = res.copy()
+
+    dx[size-1] = omega * reslocal[size-1] / matrix.FindAij(size-1, size-1)
+
+    for i in range(size-2, -1, -1):
+        reslocal[i] -= matrix.InnerProductUpperRows(dx, i)
+        dx[i] += omega * reslocal[i] / matrix.FindAij(i, i)
+
+    reslocal = res - matrix.Multiply(dx)
+    sol += dx
+
+    return sol, reslocal
+
+def SSOR(matrix:SparseMatrix, sol:np.array, res:np.array, omega:float=1.0)->np.array:
+    """
+    Evaluates the Symmetric Successive Over-Relaxation method for a given matrix, right-hand side and initial solution
+    """
+    dx1, res1 = GaussSeidelF(matrix, sol, res, omega)
+    dx2, res2 = GaussSeidelB(matrix, dx1, res1, omega)
+
+    return dx2, res2
+
+def IndirectSolver(matrix:SparseMatrix, rhs:np.array, solini:np.array, niter:int, method:callable, omega:float = 1)->tuple[float, list[float]]:
     sol = solini.copy() 
     res = rhs - matrix.Multiply(sol)
     resnorm = [np.linalg.norm(res)]
 
-    M = matrix.GetDiagonal()
     for _ in range(niter):
-        reslocal = res 
-
-        dx = omega * np.divide(reslocal, M)
-
-        reslocal -= matrix.Multiply(dx)
-        sol += dx
-
-        resnorm.append(np.linalg.norm(reslocal))
+        sol, res = method(matrix, sol, res, omega)
+        resnorm.append(np.linalg.norm(res))
 
     return sol, resnorm
 
@@ -49,13 +104,15 @@ def Main()->None:
     sol = np.zeros(len(rhs))
 
     niter = 10
-    sol, resnorm = Jacobi(matrix, rhs, sol, niter, 1.0)
+    method = GaussSeidelF
+    sol, resnorm = IndirectSolver(matrix, rhs, sol, niter, method)
+
+    print(f"Solution: {sol}")
+    print(f"Residual: {resnorm}")
 
     plt.figure()
     plt.semilogy(range(niter+1), resnorm)
     plt.show()
-
-
 
 if __name__ == "__main__":
     Main()
